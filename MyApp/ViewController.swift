f8268f50270beaa07d93c424720a58006c9ee813
Oleksandr Kataskin <zespees@gmail.com>
2024-02-20 14:31:55 +0200

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Oleksandr Kataskin on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
