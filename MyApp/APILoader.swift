5c79141ff47ed5722b1021e6dd35fde742d20aaf
Oleksandr Kataskin <zespees@gmail.com>
2024-02-21 21:15:17 +0200

    practice 2

---
+++ MyApp/APILoader.swift
@@ -0,0 +1,8 @@
+//
+//  APILoader.swift
+//  MyApp
+//
+//  Created by Oleksandr Kataskin on 20.02.2024.
+//
+
+import Foundation
---
+++ MyApp/UIImageView.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageView.swift
+//  MyApp
+//
+//  Created by Oleksandr Kataskin on 20.02.2024.
+//
+
+import UIKit
+
+class UIImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
